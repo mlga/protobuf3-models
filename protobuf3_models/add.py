f9cbd68d3190044bb968a92bd73918b63f28c702
mlga <github@mlga.io>
2020-11-11 12:42:01 +0100

    Configure CI and tools

---
+++ protobuf3_models/add.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8 -*-
+def add(a, b):  # pylint: disable=invalid-name
+    return a + b
--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,3 +1,10 @@
 # -*- coding:utf-8 -*-
+from protobuf3_models.add import add
+
+
 def test_dummy():
     assert 2 + 2 == 4
+
+
+def test_add():
+    assert add(2, 2) == 4
